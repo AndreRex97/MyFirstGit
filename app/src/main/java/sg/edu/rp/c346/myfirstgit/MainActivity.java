ce7b30947fce60dfddec1fc2c81ceb1ea046b0f3
15017103 <15017103@myrp.edu.sg>
2017-04-20 11:42:10 +0800

    Added comments to MainActivity

--- app/src/main/java/sg/edu/rp/c346/myfirstgit/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/myfirstgit/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c346.myfirstgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is a new line
     }
 }
